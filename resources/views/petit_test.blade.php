81c633c51766dacdefc1f0e52fc1c566ff0a7968
patwilli <patricesossavi@gmail.com>
2024-02-16 16:12:54 +0100

    mon deuxieme depot

---
+++ resources/views/petit_test.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum,
+    dolor sit amet consectetur adipisicing elit. Quis vel quaerat animi labore officiis,
+    earum sit placeat asperiores dicta? Inventore illum esse aliquam obcaecati veritatis suscipit cumque placeat reiciendis molestiae!
+</body>
+
+</html>
